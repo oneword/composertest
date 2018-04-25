17851e44334c6b315435c080fc83a683516058ae
oneword <kaifeng223@sina.com>
2018-04-25 17:12:22 +0800

    composer build test

---
+++ src/ComposerTest1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhang
+ * Date: 25/04/2018
+ * Time: 17:08
+ */
+
+namespace OneWord\ComposerTest;
+
+
+class ComposerTest1
+{
+    public function info()
+    {
+        echo 'test1';
+    }
+}
---
+++ src/ComposerTest2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhang
+ * Date: 25/04/2018
+ * Time: 17:09
+ */
+
+namespace OneWord\ComposerTest;
+
+
+class ComposerTest2
+{
+    public function info()
+    {
+        echo 'test2';
+    }
+}
